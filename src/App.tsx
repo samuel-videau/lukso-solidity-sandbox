a306f0c3aa9eef5fdd24d41fc4186ad0120d399e
Samuel_V <samuel@dropps.io>
2022-07-19 19:57:28 +0100

    switch to other computer

--- src/App.tsx
+++ src/App.tsx
@@ -2,46 +2,74 @@ import React from 'react';
 import './App.css';
 import {connectProfile, signMessage, web3} from "./core/web3";
 import {deployUP} from "./core/lukso/up-factory";
-import {UniversalProfile} from "./core/UniversalProfile/UniversalProfile.class";
+import {ERC725XOperationType, UniversalProfile} from "./core/UniversalProfile/UniversalProfile.class";
+import LSP7DigitalAssetInit from "./assets/artifacts/LSP7DigitalAssetInit.json";
+import {AbiItem} from "web3-utils";
 import {EthLog} from "./core/EthLogs/EthLog.class";
-import LSP7AssetArtifact from './assets/artifacts/LSP7Asset.json';
+import {tryIdentifyingContract} from "./core/contract-identification/identify-contract";
+import {StandardInterface} from "./core/contract-identification/standard-interfaces";
+import {ERC725} from "@erc725/erc725.js";
+import {InterfaceToSchema} from "./core/contract-identification/interface-to-schema";
+import {ADDRESS0} from "./utils/address0";
 
 function App() {
 
-  async function testUPClass() {
-    const universalProfile = new UniversalProfile("0xEd878fCaA2D94c22CC947a6637B8eC0a2c541A18", 'https://2eff.lukso.dev/ipfs/', web3);
-    await universalProfile.initialize();
-    // console.log(universalProfile.metadata);
-    // console.log(universalProfile.address);
-    // console.log(await universalProfile.fetchData(['0xeafec4d89fa9619884b60000abe425d64acd861a49b8ddf5c0b6962110481f38']));
-    // console.log(await universalProfile.getData(['0xeafec4d89fa9619884b60000abe425d64acd861a49b8ddf5c0b6962110481f38']));
-    // console.log(await universalProfile.fetchPermissionsOf('0xD692Ba892a902810a2EE3fA41C1D8DcD652D47Ab'));
-    // await universalProfile.setPermissionsTo('0x0C92EC41A0Aba4F33B69dA6a931A7F74C309d143', 'full');
-    // console.log(await universalProfile.fetchPermissionsOf('0x742b9DcaBE38f05CE619002029251a00F5dd0c6d'));
-    await universalProfile.subscribeLogs(0);
-    setTimeout(() => {
-      const logs: EthLog[] = universalProfile.getLogs();
-      console.log('============LOGS============');
-      console.log('Total of logs: ' + logs.length);
-      console.log('\n\n\n');
-      logs.forEach(log => {
-        console.log('==============EVENT==============');
-        console.log('Name: ' + log.getName());
-        console.log(log.getParameters());
-        console.log(log.getLog());
-      })
-    }, 6);
-  }
+    async function testUPClass() {
+      const universalProfile = new UniversalProfile("0x65068D4024B0D8dD98a95B560D290BdDB765a03b", 'https://2eff.lukso.dev/ipfs/', web3);
+      await universalProfile.initialize();
 
-  return (
-    <div>
-      <button onClick={() => {connectProfile()}}>Connect</button>
-      <button onClick={() => {signMessage()}}>Sign</button>
-      <button onClick={() => {deployUP()}}>Deploy UP</button>
-      <button onClick={() => {
-        testUPClass()}}>Deploy UP</button>
-    </div>
-  );
+
+        // await universalProfile.subscribeLogs(0);
+        // setTimeout( async () => {
+        //   const logs: EthLog[] = universalProfile.getLogs();
+        //   console.log('============LOGS============');
+        //   console.log('Total of logs: ' + logs.length);
+        //   console.log('\n\n\n');
+        //   for (const log of logs){
+        //     console.log('==============EVENT==============');
+        //     console.log('Name: ' + log.getName());
+        //     if (log.getName() === 'ContractCreated') {
+        //         const contractAddress = log.getParameters()[1].value;
+        //         const contractInterface: StandardInterface = await tryIdentifyingContract(contractAddress, web3);
+        //         console.log('Contract type: ' + contractInterface.name);
+        //         const contractSchema = InterfaceToSchema.get(contractInterface.code);
+        //         if (contractSchema) {
+        //             try {
+        //                 const erc725Y = new ERC725(contractSchema, contractAddress, web3.currentProvider, {ipfsGateway: 'https://2eff.lukso.dev/ipfs/'});
+        //                 console.log(await erc725Y.fetchData());
+        //             } catch (e) {
+        //                 // console.error(e);
+        //             }
+        //         }
+        //
+        //     }
+        //     console.log(log.getParameters());
+        //     console.log(log.getLog());
+        //   }
+        // }, 4000);
+
+
+    // const receipt = await universalProfile.execute(ERC725XOperationType.create, 0, LSP7DigitalAssetInit.bytecode);
+    // console.log(receipt);
+
+        await universalProfile.execute(ERC725XOperationType.create, 0, LSP7DigitalAssetInit.bytecode, ADDRESS0);
+        const bytecode = (new web3.eth.Contract(LSP7DigitalAssetInit.abi as AbiItem[])).methods.initialize('Test', 'TST', '0x65068D4024B0D8dD98a95B560D290BdDB765a03b', true).encodeABI();
+        const receipt = await universalProfile.execute(ERC725XOperationType.call, 0, bytecode, "0xAba58f6fCBf941AAc45688d97364C10B9FB0f5FA");
+
+    }
+
+
+
+    return (
+        <div>
+            <button onClick={() => {connectProfile()}}>Connect</button>
+            <button onClick={() => {signMessage()}}>Sign</button>
+            <button onClick={() => {deployUP()}}>Deploy UP</button>
+            <button onClick={() => {testUPClass()}}>Test UP</button>
+            <button onClick={async() => {
+                console.log(await tryIdentifyingContract('0xb9379550535F90966d6252414077D95Fc761499A', web3)) }}>Identify Contract</button>
+        </div>
+    );
 }
 
 export default App;
